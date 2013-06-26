4cb4dce727b4e2f69bd9471230fd913b461804df
tony <406504302@qq.com>
2013-06-26 17:01:15 +0800

    Signed-off-by: tony <406504302@qq.com> move UserServiceImpl.java to impl folder

--- src/main/java/com/cn/ld/modules/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/cn/ld/modules/user/service/impl/UserServiceImpl.java
@@ -1,4 +1,4 @@
-package com.cn.ld.modules.user.service;
+package com.cn.ld.modules.user.service.impl;
 
 import java.util.Arrays;
 
@@ -7,6 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Service;
 
 import com.cn.ld.modules.user.domain.User;
+import com.cn.ld.modules.user.service.UserService;
 
 @EnableCaching
 @Service

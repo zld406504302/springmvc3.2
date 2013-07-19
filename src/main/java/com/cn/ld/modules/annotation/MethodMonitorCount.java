3209e880b534ad53eb2083caa9d29f29148398d2
tony <406504302@qq.com>
2013-07-19 14:07:36 +0800

    #add jms monitor and aop config

---
+++ src/main/java/com/cn/ld/modules/annotation/MethodMonitorCount.java
@@ -0,0 +1,11 @@
+package com.cn.ld.modules.annotation;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MethodMonitorCount {
+    int value() default 1;
+}
